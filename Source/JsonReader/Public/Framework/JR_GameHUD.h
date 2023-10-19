adb9d47aa09aea3cf3b88248090c58b63e81dc23
Egor Voronov <egorvoronovev@gmail.com>
2023-10-19 14:37:46 +0300

    new(UI): initialize UI from GameHUD on BeginPlay

---
+++ Source/JsonReader/Public/Framework/JR_GameHUD.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "JR_GameHUD.generated.h"
+
+class UJR_MainMenuWD;
+
+UCLASS()
+class JSONREADER_API AJR_GameHUD : public AHUD
+{
+	GENERATED_BODY()
+
+	virtual void BeginPlay() override;
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "GameHUD|MainMenuWD")
+	void InitializeMainMenu();
+
+	UFUNCTION(Blueprintable, Category = "GameHUD|Widgets")
+	void SetFocusToWidget(UUserWidget* Widget);
+	
+protected:
+	
+	UPROPERTY(BlueprintReadOnly, Category = "GameMode|Controllers")
+	APlayerController* PlayerController = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode|Widgets")
+	TSoftClassPtr<UJR_MainMenuWD> MainMenuWDClass;
+
+	UPROPERTY(BlueprintReadOnly, Category = "GameMode|Widgets")
+	UJR_MainMenuWD* MainMenuWD = nullptr;
+};
